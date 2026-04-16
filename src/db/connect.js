29c732a1bd0f79be11de1556f30989d075f8d492
Kumar001Rishabh <rishabhkumardev01@gmail.com>
2026-04-16 15:49:56 +0530

    chore: Database Connection

--- src/db/connect.js
+++ src/db/connect.js
@@ -9,4 +9,10 @@ import mongoose from 'mongoose';
  */
 export async function connectDB(uri) {
   // Your code here
+  if(!uri){
+    throw new Error("MongoDB URI is required");
+  }
+
+  const con = await mongoose.connect(uri);
+  return con.connection;
 }
